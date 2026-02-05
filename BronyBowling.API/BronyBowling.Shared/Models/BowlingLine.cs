e82c4c6092684d4f7ffd0a3635c74c88be4a968b
Dantee <dan1ees@icloud.com>
2026-02-05 17:32:49 +0500

    add serviceBooking.API and models BowlingLane

---
+++ BronyBowling.API/BronyBowling.Shared/Models/BowlingLine.cs
@@ -0,0 +1,7 @@
+﻿namespace BronyBowling.Shared.Models;
+public class BowlingLine
+{
+    public int BowlingId { get; set; }
+    public int Number { get; set; }
+    public bool IsActive { get; set; }
+}
