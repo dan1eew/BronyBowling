1990e72ffafc9f490aba2233d7193752c88488a3
Dantee <dan1ees@icloud.com>
2026-02-05 17:38:06 +0500

    booking class

---
+++ BronyBowling.API/BronyBowling.Shared/Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace BronyBowling.Shared.Models;
+public class Booking
+{
+    public Guid BookingId { get; set; }
+    public Guid UserId { get; set; }
+    public int BowlingId { get; set; }
+    public BowlingLine Lane { get; set; }
+    public DateTime StartTime {  get; set; }
+    public DateTime EndTime { get; set; }
+    public string Status { get; set; } = "Pending";
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
