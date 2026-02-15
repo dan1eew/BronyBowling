23930b09f3e12ecc6f7f6021e07fbb31a4b1165b
Dantee <dan1ees@icloud.com>
2026-02-15 12:53:19 +0500

    - vue

--- BronyBowling.API/brony-bowling-frontend/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-vue';
-
-export default defineConfig({
-    plugins: [plugin()],
-    server: {
-      port: 5173,
-    }
-})
