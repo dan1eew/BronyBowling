c077bb248f150a4112f5a5b8249732ee2e34fb38
Dantee <dan1ees@icloud.com>
2026-02-15 14:51:41 +0500

    add Validate Booking

--- BronyBowling.API/serviceBooking.API/Validation/BookingValidator.cs
+++ BronyBowling.API/serviceBooking.API/Validation/BookingValidator.cs
@@ -1,4 +1,19 @@
 ﻿namespace serviceBooking.API.Validation;
 public class BookingValidator
 {
+    public static List<string> Validate(DateTime start, DateTime end, int laneId)
+    {
+        var errors = new List<string>();
+
+        if (laneId <= 0 || laneId > 20)
+            errors.Add("Некорректная дорожка");
+
+        if (end <= start)
+            errors.Add("Время окончания должно быть позже начала");
+
+        if (start < DateTime.UtcNow.AddMinutes(-5))
+            errors.Add("Нельзя бронировать в прошлом");
+
+        return errors;
+    }
 }
