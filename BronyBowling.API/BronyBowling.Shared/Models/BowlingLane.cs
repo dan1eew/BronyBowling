ebf5bce6fd7a1e2e26fdc9bdb494a54544166579
Dantee <dan1ees@icloud.com>
2026-02-20 12:59:32 +0500

    config postgreSQL

--- BronyBowling.API/BronyBowling.Shared/Models/BowlingLane.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BronyBowling.Shared.Models;
-
-public class BowlingLane
-{
-    public int BowlingLaneId { get; set; }   // PK
-    public int Number { get; set; }
-    public bool IsActive { get; set; }
-}
