853c63c6beaecb02abac796ebacabb7ec56802c4
Dantee <dan1ees@icloud.com>
2026-02-20 12:58:19 +0500

    admin

---
+++ BronyBowling.API/BronyBowling.Shared/Models/admin.cs
@@ -0,0 +1,8 @@
+﻿namespace BronyBowling.Shared.Models;
+public class Admin
+{
+    public Guid AdminId { get; set; }
+    public string? Login { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+}
